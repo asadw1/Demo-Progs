a20c06e32dc0a2544a6836f9987bd9efe00e1d62
csjavadev <asad.waheed.23@gmail.com>
2015-11-05 20:31:59 -0600

    Create TestCode.java

---
+++ TestCode.java
@@ -0,0 +1,10 @@
+// Quick Test...does this commit properly...
+public class TestCode
+{
+	public static void main (String[] args)
+	{
+		int i, j=0, k;
+		System.out.print("\n\n j = " + j);
+	}
+
+}
