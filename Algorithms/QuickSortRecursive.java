c94ae392a79d2e9268f27454c59dfb7d5586a84c
Asad Waheed <asad.waheed.23@gmail.com>
2016-03-02 23:00:28 -0600

    QuickSort Algorithm for integers. I'll add full bodies to the methods tomorrow. Tired.

---
+++ Algorithms/QuickSortRecursive.java
@@ -0,0 +1,31 @@
+/* Recursive Implementation of QuickSort in Java */
+/* Asad Waheed */
+/* 03-03-2016 */
+
+/* Imports. Bad practice to use *, but hey, I'm rusty! */
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+/* Not modular for now, it's just three methods */
+public class QuickSortRecursive
+{
+	/* This is the recursive method of QuickSort */
+	public static void QuickSortRecursive()
+	{
+
+	}
+
+	/* The big part of QuickSort is choosing a pivot
+	 * and deciding how to best partition the sequence,
+	 * so partition method will compute a nominally good
+	 * good pivot we can call in the QuickSort method above. */
+	public static int Partition(int [] array, int left, int right)
+	{
+
+	}
+	public static void main(String[]args)
+	{
+
+	}
+}
